84517955d8f6af5d7d5bd92841f8acecbebcb6d9
officiallisa1 <monalisaanele1@gmail.com>
2023-03-22 09:17:35 -0700

    a + b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * add - add two numbers
- * @a: para 1
- * @b: para 2
- * Return: sum
+ *add - add two numbers
+ *@a: para 1
+ *@b: para 2
+ *Return: sum
  */
 
 int add(int a, int b)
